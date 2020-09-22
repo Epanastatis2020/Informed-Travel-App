9ae4939eedc03599de8f11a90589e69b762b7ce9
ConA <65388616+Epanastatis2020@users.noreply.github.com>
2020-09-22 19:21:33 +1000

    added commented endpoints of interest to script.js

---
+++ assets/js/script.js
@@ -0,0 +1,18 @@
+// weatherAPI relevant info here
+
+// weatherAPI sample url: https://api.weatherbit.io/v2.0/current?city=Sydney&key=2583e6de5539494ca55db9ec0e80a5ee
+
+// weather API current weather endpoints of interest:
+//data[0].ob_time (last observation time)
+//data[1].datetime (current time)
+//data[0].country_code (country code)
+//data[0].state_code (state code)
+//data[1].temp (temp in celcius)
+//data[0].rh (relative humidity)
+//data[0].weather.icon (weather icon)
+//data[0].weather.code (weather code?)
+//data[0].weather.description (text weather description - maybe handy for alt)
+//data[1].uv (uvi)
+//data[1].aqi (air quality)
+//data[1].precip (rain)
+//data[1].sunrise
